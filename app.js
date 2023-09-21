71367b196697962d88fe1ef1cc50710378f1f001
ho.hung <hohung1203@gmail.com>
2023-09-21 16:27:33 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route")
 const port = 3000
 
 const app = express()
 
 app.use(cors())
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"))
+})
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    })
+})
+
 app.use("/api/contacts", contactsRouter);
 
-app.get('/', function (req, res, next) {
- res.json({msg: 'Welcome to contact book app'})
-})
-  
 module.exports = app
